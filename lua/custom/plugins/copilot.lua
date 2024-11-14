44835246795d7f1dba3daf8b8575f7eb9a4173d0
Itsuki Aoyagi <mail@yagi2.dev>
2024-11-14 13:51:07 +0900

    rearrange initial settings

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        config = function()
+            require("copilot").setup({
+                suggestion = {enabled = false},
+                panel = {enabled = false},
+                copilot_node_command = 'node'
+            })
+        end,
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        config = function ()
+            require("copilot_cmp").setup()
+        end,
+    },
+}
