4f64d1efafe6728062cb926ca9666fd1e2281998
Itsuki Aoyagi <mail@yagi2.dev>
2024-11-15 14:53:37 +0900

    rearrange lint settings

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
       }
 
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
