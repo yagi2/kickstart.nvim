d4e24a083fe631761cccfa6ca64d4f5502df0815
Itsuki Aoyagi <mail@yagi2.dev>
2024-11-15 14:08:43 +0900

    change color schema

--- init.lua
+++ init.lua
@@ -537,7 +537,7 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000,
     init = function()
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-moon'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
